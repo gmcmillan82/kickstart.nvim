7074a2dbc047499e232b8b8c8428123411e0c74e
Graham McMillan <30657475+gmcmillan82@users.noreply.github.com>
2024-06-17 17:16:17 +0200

    Change tokyonight flavour

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -36,14 +36,14 @@
 
 return {
   {
-    "folke/tokyonight.nvim",
+    'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      require("tokyonight").setup({
-        style = "moon",
-      })
-      vim.cmd[[colorscheme tokyonight]]
-    end
-  }
+      require('tokyonight').setup {
+        style = 'night',
+      }
+      vim.cmd [[colorscheme tokyonight]]
+    end,
+  },
 }
